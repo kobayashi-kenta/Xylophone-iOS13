fa500fce02e4f027d4afeeac7fa66ee41cd7c40a
kobayashi-kenta <12321klj@gmail.com>
2020-12-27 16:43:02 +0900

    finish practice

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        print("start")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1 ) {
+            sender.alpha = 1.0
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ) {
+            print("end")
+        }
+        
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
